845da8055b93db971650521799fea42b7cb2acbd
Jana Harper <jana@betterment.com>
2016-02-11 20:06:15 -0500

    Fixing...

--- app.rb
+++ app.rb
@@ -3,40 +3,40 @@ require 'sinatra/reloader' if development?
 require 'csv'
 
 get '/' do
- "Welcome to Knittable<br><br><form action='/pattern' method='post'><input type='file' name='csv_path'/><br><input type='submit'/></form>"
+  "Welcome to Knittable<br><br><form action='/pattern' method='post'><input type='file' name='csv_path'/><br><input type='submit'/></form>"
 end
 
 post '/pattern' do
-    instructions = ''
-    stitch = 'knit'
-    color = 'white'
-    stitch_count = 0
-    previous_cell = 1
-    row_count = 1
-    image = ''
-    CSV.foreach(params[:csv_path]) do |row|
-      columns = row.to_a
-      columns.each do |cell|
-        if cell != previous_cell
-          instructions = instructions + "<li>Row #{row_count}: #{stitch} #{stitch_count}</li>" unless stitch_count == 0
-          image = image + "<div style='width:10px; height: 10px; display: inline-block; background-color: #{color};'>&nbsp;</div>"
-          if stitch == 'knit'
-            color = 'blue'
-            stitch = 'purl'
-          else
-            color = 'white'
-            stitch = 'knit'
-          end
-          stitch_count = 0
-          previous_cell = cell
+  output = ''
+  stitch = 'knit'
+  color = 'white'
+  count = 0
+  previous_cell = 1
+  row_count = 1
+  image = ''
+  CSV.foreach(params[:csv_path]) do |row|
+    columns = row.to_a
+    columns.each do |cell|
+      if cell != previous_cell
+        output = output + "<li>Row #{row_count}: #{stitch} #{count}</li>" unless count == 0
+        if stitch == 'knit'
+          color = 'blue'
+          stitch = 'purl'
+        else
+          color = 'white'
+          stitch = 'knit'
         end
-        stitch_count += 1
+        count = 0
+        previous_cell = cell
       end
-      instructions = instructions + "<li>Row #{row_count}: #{stitch} #{stitch_count}</li>" unless stitch_count == 0
-      stitch = 'knit'
-      stitch_count = 0
-      previous_cell = 1
-      row_count = row_count + 1
+      image = image + "<div style='width:10px; height: 10px; display: inline-block; background-color: #{color};'>&nbsp;</div>"
+      count += 1
     end
-    "<div style='max-width:400px'>#{image}</div><ul>#{instructions}</ul>"
+    output = output + "<li>Row #{row_count}: #{stitch} #{count}</li>" unless count == 0
+    stitch = 'knit'
+    count = 0
+    previous_cell = 1
+    row_count = row_count + 1
+  end
+  "<div style='max-width:400px'>#{image}</div><ul>#{output}</ul>"
 end

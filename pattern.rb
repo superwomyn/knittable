0c3c3d80a656a56fedd98ef105cc8c587f79f9f3
Jana Harper <jana@betterment.com>
2016-02-12 13:06:55 -0500

    Let's add some tests, shall we?

--- pattern.rb
+++ pattern.rb
@@ -1,7 +1,67 @@
 class Pattern
 
-  def generate(csv)
-    raise 'CSV file is required' if csv.nil?
-  end
+  attr_accessor :csv_path
+  attr_accessor :data
 
+  def generate
+    raise 'Path to CSV file or csv string is required' if @csv_path.nil? && @data.nil?
+    @data = CSV.read(@csv_path) if @data.empty?
+    results = []
+
+    # output = ''
+    stitch = 'knit'
+    color = 'white'
+    stitch_count = 0
+    previous_cell = 1
+    row_count = 0
+    image = ''
+    column_count = 0
+    @data.each do |row|
+      # columns = row.to_a
+      column_count = row.length
+      row.each do |cell|
+        # if we're changing from one stitch to another
+        if cell != previous_cell
+          if results[row_count].nil?
+            results[row_count] = [{row: row_count, stitch: stitch, stitch_count: stitch_count}] unless stitch_count == 0
+          else
+            results[row_count].push({row: row_count, stitch: stitch, stitch_count: stitch_count}) unless stitch_count == 0
+          end
+          if stitch == 'knit'
+            color = 'blue'
+            stitch = 'purl'
+          else
+            color = 'white'
+            stitch = 'knit'
+          end
+          stitch_count = 0
+          previous_cell = cell
+        end
+
+        image = image + "<div style='width:10px; height: 10px; display: inline-block; background-color: #{color};'>&nbsp;</div>"
+        stitch_count += 1
+      end
+      if results[row_count].nil?
+        results[row_count] = [{row: row_count, stitch: stitch, stitch_count: stitch_count}] unless stitch_count == 0
+      else
+        results[row_count].push({row: row_count, stitch: stitch, stitch_count: stitch_count}) unless stitch_count == 0
+      end
+      stitch = 'knit'
+      stitch_count = 0
+      previous_cell = 1
+
+      if row_count.odd?
+        #reverse array
+        results[row_count].reverse!
+        results[row_count].each do |group|
+          if group[:stitch] == 'knit'
+            group[:stitch] = 'purl'
+          else
+            group[:stitch] = 'knit'
+          end
+        end
+      end
+      row_count = row_count + 1
+    end
+  end
 end
--- specs/pattern_spec.rb
+++ specs/pattern_spec.rb
@@ -1,12 +1,38 @@
 require './pattern'
+require 'csv'
 
 RSpec.describe Pattern do
   describe "#generate" do
 
-    context 'when there is no csv' do
-      it "raises" do
+    context 'when there is no data or csv path provided' do
+      it 'raises' do
         pattern = Pattern.new
-        expect{ pattern.generate(nil) }.to raise_error(RuntimeError)
+        expect{ pattern.generate }.to raise_error(RuntimeError)
+      end
+    end
+
+    # TODO FIXME create an actual csv file with data
+    # context 'when there is a csv file provided' do
+    #   it 'uses the csv file to populate the data' do
+    #     pattern = Pattern.new
+    #     pattern.csv_path = 'test.csv'
+    #     expect{ pattern.generate }.to raise_error(RuntimeError)
+    #   end
+    # end
+
+    context 'when the data is empty' do
+      it 'returns the correct results array' do
+        pattern = Pattern.new
+        pattern.data = [['']]
+        expect(pattern.generate).to eq [['']] # TODO FIXME this is actually wrong
+      end
+    end
+
+    context 'when the data is one cell' do
+      it 'returns the correct results array' do
+        pattern = Pattern.new
+        pattern.data = [['1']]
+        expect(pattern.generate).to eq [['1']]
       end
     end
 

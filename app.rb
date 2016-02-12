1862ca32e18e08d17998ab7d943a35e75d4deb71
Jana Harper <jana@betterment.com>
2016-02-11 19:25:08 -0500

    All the things.

--- app.rb
+++ app.rb
@@ -1,10 +1,42 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'csv'
 
 get '/' do
-  "Welcome to Knittable<br><br><form action='/pattern' method='post'><textarea name='csv'></textarea><br><input type='submit'/></form>" 
+ "Welcome to Knittable<br><br><form action='/pattern' method='post'><input type='file' name='csv_path'/><br><input type='submit'/></form>"
 end
 
 post '/pattern' do
-  	"Hello #{params[:csv]}!"
+    output = ''
+    stitch = 'knit'
+    color = 'white'
+    count = 0
+    previous_cell = 1
+    row_count = 1
+    image = ''
+    CSV.foreach(params[:csv_path]) do |row|
+      columns = row.to_a
+      columns.each do |cell|
+        if cell != previous_cell
+          output = output + "<li>Row #{row_count}: #{stitch} #{count}</li>" unless count == 0
+          if stitch == 'knit'
+            color = 'blue'
+            stitch = 'purl'
+          else
+            color = 'white'
+            stitch = 'knit'
+          end
+          count = 0
+          previous_cell = cell
+        end
+        image = image + "<div style='width:10px; height: 10px; display: inline-block; background-color: #{color};'>&nbsp;</div>"
+        count += 1
+      end
+      output = output + "<li>Row #{row_count}: #{stitch} #{count}</li>" unless count == 0
+      stitch = 'knit'
+      count = 0
+      previous_cell = 1
+      row_count = row_count + 1
+    end
+    "<div style='max-width:400px'>#{image}</div><ul>#{output}</ul>"
 end

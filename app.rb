9cd649b09536baa3d97ca8bcb724de84ab34722b
Jana Harper <jana@betterment.com>
2016-02-11 23:16:00 -0500

    Refactoring.

--- app.rb
+++ app.rb
@@ -17,14 +17,20 @@ post '/pattern' do
   color = 'white'
   stitch_count = 0
   previous_cell = 1
-  row_count = 1
+  row_count = 0
   image = ''
   column_count = 0
+  results = []
   CSV.foreach("csv/#{params[:csv_path]}") do |row|
     columns = row.to_a
     column_count = columns.length
     columns.each do |cell|
       if cell != previous_cell
+        if results[row_count].nil?
+          results[row_count] = [{row: row_count, stitch: stitch, stitch_count: stitch_count}] unless stitch_count == 0
+        else
+          results[row_count].push({row: row_count, stitch: stitch, stitch_count: stitch_count}) unless stitch_count == 0
+        end
         output = output + "<li>Row #{row_count}: #{stitch} #{stitch_count}</li>" unless stitch_count == 0
         if stitch == 'knit'
           color = 'blue'
@@ -39,6 +45,11 @@ post '/pattern' do
       image = image + "<div style='width:10px; height: 10px; display: inline-block; background-color: #{color};'>&nbsp;</div>"
       stitch_count += 1
     end
+    if results[row_count].nil?
+      results[row_count] = [{row: row_count, stitch: stitch, stitch_count: stitch_count}] unless stitch_count == 0
+    else
+      results[row_count].push({row: row_count, stitch: stitch, stitch_count: stitch_count}) unless stitch_count == 0
+    end
     output = output + "<li>Row #{row_count}: #{stitch} #{stitch_count}</li>" unless stitch_count == 0
     stitch = 'knit'
     stitch_count = 0
@@ -46,4 +57,5 @@ post '/pattern' do
     row_count = row_count + 1
   end
   "<div style='max-width:#{column_count * 10}px;'>#{image}</div><ul>#{output}</ul>"
+  #results.join(' ')
 end
